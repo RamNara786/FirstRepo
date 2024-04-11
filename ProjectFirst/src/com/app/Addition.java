26057cc9a14189d9b966c12587612a2bc97a35e7
RamNara786 <balunara4@gmail.com>
2024-04-11 13:01:13 -0400

    First Project commit

---
+++ ProjectFirst/src/com/app/Addition.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int A =10;
+		int B =20;
+		int result =  A+ B;
+		System.out.print("The sum of A +B is " + result);	
+
+	}
+
+}
